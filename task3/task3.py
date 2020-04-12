432522004a8c9b861dde42e50953ba9c2140e03a
LomtiQ1001 <47700444+LomtiQ1001@users.noreply.github.com>
2020-04-12 15:15:06 +0300

    Add files via upload

---
+++ task3/task3.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+data1 = []
+data2 = []
+data3 = []
+data4 = []
+data5 = []
+data = [data1, data2, data3, data4, data5]
+text = ["Cash1.txt", "Cash2.txt", "Cash3.txt", "Cash4.txt", "Cash5.txt"]
+
+def openf(txt_files):
+    datas1 = []
+    datas2 = []
+    datas3 = []
+    datas4 = []
+    datas5 = []
+    datas = [datas1, datas2, datas3, datas4, datas5]
+    for i in range(len(txt_files)):
+        with open(txt_files[i]) as f:
+            for line in f:
+                datas[i].append([float(x) for x in line.split()])
+            for j in range(len(datas[i])):
+                for k in range(len(datas[i][j])):
+                    data[i].append(datas[i][j][k])
+
+def find_max():
+    final = []
+    for i in range(len(data1)):
+        final.append(data1[i] + data2[i] + data2[i] + data2[i] + data2[i])
+    rezult = max(final)
+    for i in range(len(data1)):
+        if rezult == final[i]:
+            print(i+1)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        os.chdir(sys.argv[1])
+        openf(text)
+        find_max()
