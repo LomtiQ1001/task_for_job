1e247c5667398aba6503a69b830f8c1789aecf5b
LomtiQ1001 <47700444+LomtiQ1001@users.noreply.github.com>
2020-04-12 16:58:44 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -5,6 +5,7 @@ import numpy as np
 datas = []
 
 def openf(name_file: str):
+    #  сканируем файл и записываем его значения в список в удобном формате
     with open(name_file) as f:
         data = []
         for line in f:
@@ -14,11 +15,13 @@ def openf(name_file: str):
                 datas.append(data[i][j])
 
 def percentile(lst):
+    # узнаём перцентель и выводим
     perc = np.percentile(lst, 90)
     perc = f'{perc:.{2}f}'
     print(perc)
 
 def median (lst):
+    # узнаём медиану и выводим
     n=len(lst)
     s=sorted(lst)
     k=n//2
@@ -32,18 +35,21 @@ def median (lst):
         print(med)
 
 def maximum (lst):
+    # находим максимум и выводим
     s = sorted(lst)
     max = len(s)
     max = f'{max:.{2}f}'
     print(max)
 
 def minimum (lst):
+    # находим минимум и выводим        
     s = sorted(lst)
     min = s[0]
     min = f'{min:.{2}f}'
     print(min)
 
 def mean (lst):
+    # находим среднее и выводим
     sum = 0
     i = 0
     for i in range(len(lst)):
@@ -52,11 +58,16 @@ def mean (lst):
     rezult = f'{rezult:.{2}f}'
     print(rezult)
 
+# начало программы
 if __name__ == "__main__":
+    # проверяем есть ли аргументы
     if len (sys.argv) > 1:
+        # в случае если файл лежит в отличном от скрипта каталоге, то записываем путь к файлу и имя файла в переменные
         way_to_file = os.path.dirname(sys.argv[1])
         name_f = os.path.basename(sys.argv[1])
+        # открыли каталог с файлом        
         os.chdir(way_to_file)
+        # запустили функции для обработки
         openf(name_f)
         percentile(datas)
         median(datas)
